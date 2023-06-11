da5120d058b121a12ac2eaebe0bad71f671a04df
ondren <andreykl123321@gmail.com>
2023-06-11 21:20:38 +0300

    added tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl.js');
+const {normalizeURL, getURLsFromHTML} = require('./crawl.js');
 const{expect, test} = require('@jest/globals');
 
 
@@ -27,5 +27,76 @@ test("normalizeURL strip http", ()=>{
     const input = "http://website.com/path";
     const actual = normalizeURL(input);
     const expected = "website.com/path";
+    expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML absolute", ()=>{
+    const inputHTMLBpdy = `
+    <html>
+        <body>
+            <a href="https://website.com/path/">
+                Website
+            </a>
+        
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://website.com/path/";
+    const actual = getURLsFromHTML(inputHTMLBpdy, inputBaseURL);
+    const expected = ["https://website.com/path/"];
+    expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", ()=>{
+    const inputHTMLBpdy = `
+    <html>
+        <body>
+            <a href="/path/">
+                Website
+            </a>
+        
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://website.com";
+    const actual = getURLsFromHTML(inputHTMLBpdy, inputBaseURL);
+    const expected = ["https://website.com/path/"];
+    expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML both", ()=>{
+    const inputHTMLBpdy = `
+    <html>
+        <body>
+            <a href="https://website.com/path1/">
+                Website path 1
+            </a>
+            <a href="/path2/">
+                Website path 2
+            </a>
+        
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://website.com";
+    const actual = getURLsFromHTML(inputHTMLBpdy, inputBaseURL);
+    const expected = ["https://website.com/path1/", "https://website.com/path2/"];
+    expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML invalid", ()=>{
+    const inputHTMLBpdy = `
+    <html>
+        <body>
+            <a href="invalid">
+                Invalid URL
+            </a>
+        
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://website.com";
+    const actual = getURLsFromHTML(inputHTMLBpdy, inputBaseURL);
+    const expected = [];
     expect(actual).toEqual(expected);
 });
